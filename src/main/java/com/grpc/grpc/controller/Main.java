7a7fbe632ba021ed798a14f108b6d6bda153bf23
fahmi <fahmi@code.id>
2019-01-30 15:15:25 +0700

    remove private

--- src/main/java/com/grpc/grpc/controller/Main.java
+++ src/main/java/com/grpc/grpc/controller/Main.java
@@ -1,9 +1,10 @@
 package com.grpc.grpc.controller;
 
 import com.grpc.grpc.config.security.model.UserDetailModel;
-import com.grpc.grpc.exception.InternalAppsException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/berak")
 public class Main {
-    private UserDetailModel userDetailModel;
-
     @RequestMapping(method = RequestMethod.GET, value = "")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity index() {
-        throw new InternalAppsException("berak");
-//        this.userDetailModel = (UserDetailModel) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        return ResponseEntity.status(HttpStatus.OK).body("string" + userDetailModel.getUsername());
+        UserDetailModel userDetailModel = (UserDetailModel) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.status(HttpStatus.OK).body("string" + userDetailModel.getUsername());
     }
 }
