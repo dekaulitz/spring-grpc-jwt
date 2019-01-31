a0cf5b705555fe0abc0a8cfb7dcdb07cfe149cf4
fahmi <fahmi@code.id>
2019-01-31 15:33:09 +0700

    add query native

--- src/main/java/com/github/spring/grpc/repository/UserRepository.java
+++ src/main/java/com/github/spring/grpc/repository/UserRepository.java
@@ -2,8 +2,14 @@ package com.github.spring.grpc.repository;
 
 import com.github.spring.grpc.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    @Query(value = "SELECT u FROM users u WHERE u.email = ?1", nativeQuery = true)
+    UserEntity findByEmail(String email);
+
+    UserEntity findByName(String name);
 }
