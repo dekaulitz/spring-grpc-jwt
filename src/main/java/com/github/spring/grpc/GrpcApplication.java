87ccbba61e7bc9566a2f1e9597c99f90f1876082
fahmi <fahmi@code.id>
2019-01-31 15:34:04 +0700

    add dummy data

--- src/main/java/com/github/spring/grpc/GrpcApplication.java
+++ src/main/java/com/github/spring/grpc/GrpcApplication.java
@@ -1,14 +1,13 @@
 package com.github.spring.grpc;
 
-import com.github.spring.grpc.config.security.jwt.manager.JwtManager;
 import com.github.spring.grpc.entity.UserEntity;
 import com.github.spring.grpc.repository.UserRepository;
-import com.github.spring.grpc.view.model.JwtUserModel;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @Log4j2
@@ -22,19 +21,16 @@ public class GrpcApplication {
     public CommandLineRunner dummyData(UserRepository userRepository) {
         return (args -> {
             for (int i = 0; i < 10; i++) {
+                BCryptPasswordEncoder password = new BCryptPasswordEncoder();
+                String pass = password.encode("fahmisulaiman");
                 UserEntity user = new UserEntity();
-                user.setName("fahmi sulaiman" + i);
+                user.setName("fahmi" + i);
                 user.setEmail("sulaimanfahmi@gmail.com" + i);
                 user.setAddres("Apl Tower lt.26" + i);
+                user.setRole("ADMIN");
+                user.setPassword(pass);
                 userRepository.save(user);
             }
-            JwtUserModel jwtUserModel = new JwtUserModel();
-            jwtUserModel.setId(123123123);
-            jwtUserModel.setUsername("fahmi");
-
-            jwtUserModel.setRole("BERAK");
-            String token = JwtManager.generateToken(jwtUserModel);
-            log.info(token);
         });
     }
 
