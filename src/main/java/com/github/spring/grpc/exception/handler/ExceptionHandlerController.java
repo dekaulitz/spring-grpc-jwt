2d6add513a9389461a778ef2c75a891eb734e506
fahmi <fahmi@code.id>
2019-01-31 15:35:13 +0700

    add exception handling

--- src/main/java/com/github/spring/grpc/exception/handler/ExceptionHandlerController.java
+++ src/main/java/com/github/spring/grpc/exception/handler/ExceptionHandlerController.java
@@ -6,8 +6,11 @@ import com.github.spring.grpc.exception.InternalAppsException;
 import com.github.spring.grpc.view.model.ErrorModel;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -22,5 +25,19 @@ public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
         ErrorModel errorModel = new ErrorModel(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(),
                 ex.getLocalizedMessage());
         return new ResponseEntity<>(errorModel, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorModel errorModel = new ErrorModel(HttpStatus.METHOD_NOT_ALLOWED, ex.getMessage(),
+                ex.getLocalizedMessage());
+        return new ResponseEntity<>(errorModel, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorModel errorModel = new ErrorModel(HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage(),
+                ex.getLocalizedMessage());
+        return new ResponseEntity<>(errorModel, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 }
