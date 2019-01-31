84f0ed1e6cafdf593579c83bbd4f1fdf19366ec1
fahmi <fahmi@code.id>
2019-01-31 15:35:00 +0700

    add jwt user model

--- src/main/java/com/github/spring/grpc/view/model/JwtUserModel.java
+++ src/main/java/com/github/spring/grpc/view/model/JwtUserModel.java
@@ -1,21 +1,23 @@
 package com.github.spring.grpc.view.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@Component
+
 public class JwtUserModel {
     private long id;
     private String username;
     private String role;
+    private String token;
+
+    public JwtUserModel(long id, String username, String token, String role) {
+        this.id = id;
+        this.username = username;
+        this.token = token;
+        this.role = role;
+    }
 }
